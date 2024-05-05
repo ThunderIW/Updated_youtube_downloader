bce2ff3e9a64fbcddbede5ac3f0b0d806d99faff
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2024-05-05 13:20:08 -0700

    removed ffmpeg

--- updatedYoutubeDownloadWeb.py
+++ updatedYoutubeDownloadWeb.py
@@ -34,25 +34,9 @@ if submitted:
     audio = yt.streams.filter(only_audio=True).first()
     video.download(output_path='downloads', filename="Downloaded_video.mp4")
     audio.download(output_path='downloads', filename="Downloaded_video.mp3")
-    cmd = [
-        'ffmpeg',
-        '-i', "downloads/Downloaded_video.mp4",  # Input video file
-        '-i', "downloads/Downloaded_video.mp3",  # Input audio file
-        '-c:v', 'copy',  # Copy the video stream
-        '-c:a', 'aac',  # Encode audio to AAC
-        '-strict', 'experimental',
-        '-map', '0:v:0',  # Map video stream from the first input
-        '-map', '1:a:0',
-        "downloads/video.mp4"  # Map audio stream from the second input
-        # Output file path
-    ]
+    video_clip = VideoFileClip("downloads/Downloaded_video.mp4")
+    audio_clip = AudioFileClip("downloads/Downloaded_video.mp3")
+    final_clip = video_clip.set_audio(audio_clip)
+    final_clip.write_videofile("output_video.mp4", codec="libx264", audio_codec="aac")
 
-    process = subprocess.run(cmd, capture_output=True, text=True)
-    if process.returncode == 0:
-        st.success("Video and audio have been merged successfully!")
-        with open("downloads/video.mp4", "rb") as file:
-            btn = st.download_button(label="Download", data=file, file_name=f"{title}.mp4", mime="video/mp4")
-    else:
-        st.error("Failed to merge video and audio: " + process.stderr)
-
-    os.remove("downloads/video.mp4")
+    os.remove("downloads/output_video.mp4")
